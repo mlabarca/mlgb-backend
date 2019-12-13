a483c5f0d23cc7364876b1dd97d8fa9f537cf8b4
Mauricio Labarca <mauriciolabarca4tw@gmail.com>
2019-12-12 23:34:22 -0300

    Add failing test for jobs endpoint

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -3,6 +3,17 @@ require 'rails_helper'
 RSpec.describe JobsController, type: :controller do
 
   describe 'GET #search' do
+    let(:gb_json){
+      File.read(Rails.root.join('spec', 'fixtures', 'jobs_response.json'))
+    }
+
+    let(:parsed_gb_json) { JSON.parse(gb_json) }
+
+    before :each do
+      response = Typhoeus::Response.new(code: 200, body: gb_json)
+      Typhoeus.stub(/#{Rails.configuration.services[:gb_endpoint]}/).and_return(response)
+    end
+
     it 'returns http success' do
       get :search
       expect(response).to have_http_status(:success)
@@ -11,7 +22,7 @@ RSpec.describe JobsController, type: :controller do
     it 'returns a filtered json array' do
       get :search
       response_json = JSON.parse(response.body)
-      expect(response_json['jobs']).to eq []
+      expect(response_json['jobs']).to eq parsed_gb_json['jobs']
     end
   end
 
